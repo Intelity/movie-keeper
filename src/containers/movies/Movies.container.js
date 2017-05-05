e569ef19ae8b63d961b468c4851396a32e85549b
Iurii Kobernyk <koiu@ciklum.com>
2017-05-05 04:24:13 +0300

    delete local components

--- src/containers/movies/Movies.container.js
+++ src/containers/movies/Movies.container.js
@@ -3,32 +3,32 @@ import { MoviesComponent } from '../../components/movies/Movies.component';
 
 
 class MoviesContainer extends Component {
-
+  
   constructor(props) {
     super(props);
-
+    
     this.state = {
       movies: {},
       showAddMovie: false,
     };
-
+    
     this.addNewMovie = this.addNewMovie.bind(this);
     this.hideAddMovieComponent = this.hideAddMovieComponent.bind(this);
     this.showAddMovieComponent = this.showAddMovieComponent.bind(this);
   }
-
+  
   componentWillMount() {
     const movieColl = JSON.parse(window.localStorage.getItem('movie-collection') || '{ "movies": [] }');
     this.setState({
       movies: movieColl.movies,
     });
   }
-
+  
   addNewMovie(newMovie) {
     const updateMovie = {};
     updateMovie[Object.keys(this.state.movies).length + 1] = newMovie;
     const movies = Object.assign({}, this.state.movies, updateMovie);
-
+    
     this.setState({
       movies: movies,
       showAddMovie: false,
@@ -37,19 +37,19 @@ class MoviesContainer extends Component {
       movies: movies
     }));
   }
-
+  
   hideAddMovieComponent() {
     this.setState({
       showAddMovie: false
     });
   }
-
+  
   showAddMovieComponent() {
     this.setState({
       showAddMovie: true,
     });
   }
-
+  
   render() {
     return (
       <MoviesComponent {...this.state}
